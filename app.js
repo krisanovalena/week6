90b18cf10334d141fcdb18d45f79d8c9db5bd89c
Elena Krisanova <krisanovalena@gmail.com>
2021-11-29 20:35:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-export default function appScr(express, bodyParser, fs, crypto, http) {
+export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
     const app = express();
     const hu = {'Content-Type':'text/html; charset=utf-8'}
-    const CORS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE',
-      };
     let headers = {
         'Content-Type':'text/plain',
         ...CORS
     }
-    return app
+    app
         .use(bodyParser.urlencoded({extended:true}))       
-        .all('/login/', (req, res) => {
-            res.set(headers)
-            res.send('lenekke');
+        .all('/login/', r => {
+            r.res.set(headers).send('lenekke');
         })
-        .all('/code/', (req, res) => {
-            res.set(headers)
+        .all('/code/', r => {
+            r.res.set(headers)
             fs.readFile(import.meta.url.substring(7),(err, data) => {
                 if (err) throw err;
-                res.end(data);
+                r.res.end(data);
               });           
         })
-        .all('/sha1/:input/', (req, res) => {
-            res.set(headers)
-            let shasum = crypto.createHash('sha1')
-            res.send(shasum.update(req.params.input).digest('hex'))
+        .all('/sha1/:input/', r => {
+            r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
         })
         .get('/req/', (req, res) =>{
             res.set(headers);
@@ -37,15 +30,29 @@ export default function appScr(express, bodyParser, fs, crypto, http) {
                 res.send(data)
             })
         })
-        .post('/req/', (req, res) =>{
-            res.set(headers);
-            let data = '';
-            http.get(req.body.addr, async function(response) {
-                await response.on('data',function (chunk){
-                    data+=chunk;
-                }).on('end',()=>{})
-                res.send(data)
-            })
+        .post('/req/', r =>{
+            r.res.set(headers);
+            const {addr} = req.body;
+            r.res.send(addr)
+        })
+        .post('/insert/', async r=>{
+            r.res.set(headers);
+            const {login,password,URL}=r.body;
+            const newUser = new User({login,password});
+            try{
+                await m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
+                try{
+                    await newUser.save();
+                    r.res.status(201).json({'Добавлено: ':login});
+                }
+                catch(e){
+                    r.res.status(400).json({'Ошибка: ':'Нет пароля'});
+                }
+            }
+            catch(e){
+                console.log(e.codeName);
+            }      
         })
         .use(({res:r})=>r.status(404).set(hu).send('lenekke'))
+    return app;
 }
